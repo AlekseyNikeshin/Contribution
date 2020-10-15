4ee79beea9195868ab28ee83d073b957f467c24f
unknown <aleksey_nikeshin@mail.ru>
2020-10-15 18:45:32 +0300

    contribution

---
+++ src/Contribution.java
@@ -0,0 +1,9 @@
+public class Contribution {
+    public static void main(String[] args) {
+        int interestRate = 3;
+        int sum = 5000;
+        double income = interestRate * sum/100.0;
+        System.out.println(income);
+
+    }
+}
